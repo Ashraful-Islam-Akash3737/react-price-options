6736108bcc80e24dace77578674e6d875f2c3f2c
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-03-18 23:24:23 +0600

    install tailwind and daisyUI and make a nav bar with the help of daisy UI.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+
+import './App.css'
+import DaisyNav from './Components/DaisyNav/DaisyNav'
+
+function App() {
+
+  return (
+    <>
+      <DaisyNav></DaisyNav>
+      <h1 className='text-6xl bg-rose-700'>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
